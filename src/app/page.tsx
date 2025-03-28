772e12ad11b804e71c12937f5af45c3ccf463af3
Erik <erik.g.ingebrigtsen@gmail.com>
2025-03-28 20:02:26 +0100

    basic swipe card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import currentLoggedInUser from "@/lib/currentLoggedInUser";
 import SwipeCard from "@/component/swipecard";
+<<<<<<< HEAD
 
+=======
+>>>>>>> 6ceefaf (basic swipe card)
 export default async function Home() {
   const user = await currentLoggedInUser();
   console.log(user.attributes["sko"]);
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 6ceefaf (basic swipe card)
   return (
     <div className="w-full h-screen">
       <SwipeCard/>
