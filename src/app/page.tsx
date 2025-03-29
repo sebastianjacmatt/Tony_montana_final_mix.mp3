194d39a07923d68e58d694c611db703bdddd3512
Tony Lam <Tonylam750@outlook.com>
2025-03-29 13:07:20 +0100

    null checker for user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import currentLoggedInUser from "@/lib/currentLoggedInUser";
 
 export default async function Home() {
   const user = await currentLoggedInUser();
-  console.log(user.attributes["sko"]);
+  console.log(user?.attributes["sko"]);
 
   return (
     <div className="w-full h-screen">
