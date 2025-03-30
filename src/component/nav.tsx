c3891583944ad35904d7944157484c53934fafd1
SebastianJM <sebastianjacmatt@gmail.com>
2025-03-30 11:12:48 +0200

    changed a tags to Link

--- src/component/nav.tsx
+++ src/component/nav.tsx
@@ -6,27 +6,28 @@ import {
   ArrowLeftRight,
 } from "lucide-react";
 import { logout } from "@/app/login/actions";
+import Link from "next/link";
 
 export default function Nav() {
   return (
     <nav className="fixed bottom-0 w-full">
-      <div className="flex bg-gradient-to-r from-emerald-800 to-[#A7F3D0] items-center justify-center text-black p-4 gap-16">
-        <a href="/chat" className="flex flex-col items-center">
+      <div className="flex bg-white items-center justify-center text-black p-4 gap-16">
+        <Link href="/chat" className="flex flex-col items-center">
           <MessageCircleHeart className="w-8 h-8" />
           Matches
-        </a>
-        <a href="/" className="flex flex-col items-center">
+        </Link>
+        <Link href="/" className="flex flex-col items-center">
           <House className="w-8 h-8" />
           Home
-        </a>
-        <a href="/swipe" className="flex flex-col items-center">
+        </Link>
+        <Link href="/swipe" className="flex flex-col items-center">
           <ArrowLeftRight className="w-8 h-8" />
           Swipe
-        </a>
-        <a href="/profile" className="flex flex-col items-center">
+        </Link>
+        <Link href="/profile" className="flex flex-col items-center">
           <CircleUser className="w-8 h-8" />
           Profile
-        </a>
+        </Link>
         <button
           className="flex flex-col items-center text-red-500"
           onClick={logout}
